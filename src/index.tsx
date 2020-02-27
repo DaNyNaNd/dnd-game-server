1ef95de5dd65d468ce1f1b19e99aa239d5e3ac93
ALTARUM/kroot <kevin.root@altarum.org>
2020-02-27 16:39:58 -0500

    First attempt at layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Sidebar />, document.getElementById("sidebar"));
+ReactDOM.render(<Header />, document.getElementById("header"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
