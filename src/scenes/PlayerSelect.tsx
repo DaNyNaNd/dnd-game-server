8e206167f5bd6299d9d6e87a51734a0f041f050e
ALTARUM/kroot <kevin.root@altarum.org>
2020-03-03 16:40:41 -0500

    Playercard background image

--- src/scenes/PlayerSelect.tsx
+++ src/scenes/PlayerSelect.tsx
@@ -8,49 +8,49 @@ const PlayerSelect = ({name= "NO NAME", level = 0, race= "N/A", playerclass="No 
             <header className="App-header">
         <div className="row row-sm">
                     <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
+                        <div className="card card-inverse text-white">
+                                <div className="card-body player-card">
+                                    <h4 className="card-title">{name}</h4>
+                                    <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
+                                </div>
+                  </div>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
                         <div className="card bg-dark text-white">
-                            <div className="card-body">
-                                <h5 className="card-title">{name}</h5>
+                            <div className="card-body player-card">
+                                <h4 className="card-title">{name}</h4>
                                 <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
                         <div className="card bg-dark text-white">
-                            <div className="card-body">
-                                <h5 className="card-title">{name}</h5>
+                            <div className="card-body player-card">
+                                <h4 className="card-title">{name}</h4>
                                 <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
                         <div className="card bg-dark text-white">
-                            <div className="card-body">
-                                <h5 className="card-title">{name}</h5>
+                            <div className="card-body player-card">
+                                <h4 className="card-title">{name}</h4>
                                 <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
                         <div className="card bg-dark text-white">
-                            <div className="card-body">
-                                <h5 className="card-title">{name}</h5>
+                            <div className="card-body player-card">
+                                <h4 className="card-title">{name}</h4>
                                 <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
                         <div className="card bg-dark text-white">
-                            <div className="card-body">
-                                <h5 className="card-title">{name}</h5>
-                                <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-4 mb-4">
-                        <div className="card bg-dark text-white">
-                            <div className="card-body">
-                                <h5 className="card-title">{name}</h5>
+                            <div className="card-body player-card">
+                                <h4 className="card-title">{name}</h4>
                                 <p className="card-text">Lvl {level} | {race} | {playerclass}</p>
                             </div>
                         </div>
